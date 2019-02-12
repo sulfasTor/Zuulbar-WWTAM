2b24da88d7b1ffa2baa9b6130a7e03973a9b23f2
sulfastor <torresmoisesa@gmail.com>
2019-02-12 23:38:44 +0100

    fc

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String args[]) {
+    Game quizz = new Game();
+
+    quizz.play();
+  }
+}
---
+++ Room.java
@@ -0,0 +1,91 @@
+/**
+ * Class Room - a room in an adventure game.
+ *
+ * This class is part of the "World of Zuul" application. 
+ * "World of Zuul" is a very simple, text based adventure game.  
+ *
+ * A "Room" represents one location in the scenery of the game.  It is 
+ * connected to other rooms via exits.  The exits are labelled north, 
+ * east, south, west.  For each direction, the room stores a reference
+ * to the neighboring room, or null if there is no exit in that direction.
+ * 
+ * @author  Michael Kolling and David J. Barnes
+ * @version 2006.03.30
+ */
+
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Iterator;
+
+public class Room 
+{
+  public String description;
+  public HashMap<String, Room> exits;
+  
+  /**
+   * Create a room described "description". Initially, it has
+   * no exits. "description" is something like "a kitchen" or
+   * "an open court yard".
+   * @param description The room's description.
+   */
+  public Room(String description) 
+  {
+    this.description = description;
+    this.exits =  new HashMap<String, Room>(4);
+  }
+
+  /**
+   * Define the exits of this room.  Every direction either leads
+   * to another room or is null (no exit there).
+   * @param north The north exit.
+   * @param east The east east.
+   * @param south The south exit.
+   * @param west The west exit.
+   */
+  public void setExits(Room north, Room east, Room south, Room west) 
+  { 
+    if (north != null)
+      this.exits.put("north", north);
+
+    if (east != null)
+      this.exits.put("east", east);
+
+    if (south != null)
+      this.exits.put("south", south);
+
+    if (west != null)
+      this.exits.put("west", west);
+  }
+
+  /**
+   * @return The description of the room.
+   */
+  public String getDescription()
+  {
+    return description;
+  }
+
+  /**
+   * @return The description of the room with more details.
+   */
+  public String getLongDescription()
+  {
+    return "You are at " + description + "\n" + getExitString();
+  }
+
+  
+  /**
+   * @return A string containing the available exits
+   */
+
+  public String getExitString() {
+    String returnString = "Exits: "; // Chaîne de charactères renvoyée
+    Set<String> keys = this.exits.keySet(); // Recupère l'ensemble des clés du Hash [
+    for (String exit : keys)           // Ajoute les clés a la chaîne
+      returnString += exit + ' ';
+
+    return returnString;
+  }
+
+
+}
